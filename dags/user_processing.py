5368f05402bd7f3ffb19f4958d7a15e5057ad1bc
ohikendoit <ohikendoit@gmail.com>
2023-07-11 23:30:56 +0900

    initial push of the local environment

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -15,9 +15,9 @@ def _process_user(ti):
         'firstname': user['name']['first'],
         'lastname': user['name']['last'],
         'country': user['location']['country'],
-        'username': user['login']['username']
+        'username': user['login']['username'],
         'password': user['login']['password'],
-        'email': user['email']})
+        'email': user['email'] })
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
 
