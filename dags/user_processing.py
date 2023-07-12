c973f2aed81028c3ab66aff4a7847962efd1166a
ohikendoit <ohikendoit@gmail.com>
2023-07-12 23:38:51 +0900

    minor changes on the user_processing

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,14 +1,15 @@
 from airflow import DAG
-from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 import json
 from pandas import json_normalize
+from datetime import datetime
 
-def _process_user(ti):
+def _process_user(ti): #ti as in task instance
     user = ti.xcom_pull(task_ids="extract_user")
     user = user['results'][0]
     processed_user = json_normalize({
@@ -20,8 +21,14 @@ def _process_user(ti):
         'email': user['email'] })
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
+def _store_user():
+    hook = PostgresHook(postgres_conn_id='postgres')
+    hook.copy_expert(
+        sql="COPY users FROM stdin WITH DELIMITER as ','",
+        filename='/tmp/processed_user.csv'
+    )
 
-with DAG('user_processing', start_date=(2023, 1, 1),
+with DAG('user_processing', start_date=datetime(2023, 1, 1),
          schedule_interval='@daily', catchup=False) as dag:
     
     create_table = PostgresOperator(
@@ -56,4 +63,12 @@ with DAG('user_processing', start_date=(2023, 1, 1),
     process_user = PythonOperator(
         task_id = 'process_user',
         python_callable = _process_user
+    ) #function you would like to call
+
+    store_user =PythonOperator(
+        task_id='store_user'
+        python_callable=_store_user
     )
+
+    create_table >> is_api_available >> extract_user >> process_user >> store_user
+    
