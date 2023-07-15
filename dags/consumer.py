037d1a93eeed8bb222d6c1fa5f219e0f413d0787
ohikendoit <ohikendoit@gmail.com>
2023-07-15 22:43:15 +0900

    group and parallel dags

--- dags/consumer.py
+++ dags/consumer.py
@@ -4,10 +4,11 @@ from airflow.decorators import task
 from datetime import datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file_2.txt")
 
 with DAG(
     dag_id = "consumer",
-    schedule = [my_file], #Use it as a schedule value
+    schedule = [my_file, my_file_2], #Use it as a schedule value
     start_date = datetime(2023, 1, 1),
     catchup = False
 ):
--- dags/producer.py
+++ dags/producer.py
@@ -4,16 +4,22 @@ from airflow.decorators import task
 from datetime import datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file.txt")
 
 with DAG(
     dag_id = "producer",
     schedule = "@daily",
-    start_date = datetime(2023, 1, 1)
+    start_date = datetime(2023, 1, 1),
     catchup = False
 ):
 
     @task(outlets=[my_file])
     def update_dataset():
+        with open(my_file.uri, "a+") as f:
+            f.write("producer update")
+    
+    @task(outlets=[my_file_2])
+    def update_dataset_2():
         with open(my_file.uri, "a+") as f:
             f.write("producer update")
     
